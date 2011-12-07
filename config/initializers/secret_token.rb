58e334425d0959bd0c67dd73e0c046f0b0e503e9
Kevin Peterson <kevin@kdpeterson.net>
2011-12-07 10:19:17 -0800

    fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hippocket::Application.config.secret_token = '5af4e705c567885c8d23ca7acbe88a3bb6f5a16656f8edf185d255e5bee479ad26f50acd606363c51b25b08b53ad74730265350f5ddf7d146d9da20de4d5e8ae'
