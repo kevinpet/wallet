b57df38bc8e07dd17270e3bdef119cd0d51d2894
Kevin Peterson <kevin@kdpeterson.net>
2011-12-27 12:00:18 -0800

    main UI single selected password vault at a time

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,1 +1,5 @@
 /* http://docs.angularjs.org/#!angular.filter */
+angular.filter("titleVault", function(vault) {
+    var locked =  vault.locked ? "locked" : "unlocked";
+    return vault.resource.name + " (" + locked + ")";
+});
