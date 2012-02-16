b05ca84ae4674f3563799306f611b8fb19b11ea6
Kevin Peterson <kevin@kdpeterson.net>
2012-02-15 22:43:50 -0800

    improved show/hide, fix save and lock

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -2,3 +2,14 @@
 angular.filter("dirtyTitle", function(vault) {
     return vault.resource.name + (vault.dirty ? "*" : "");
 });
+var expand = function(start, active) {
+    return function(toggle) {
+	if (toggle) {
+	    return active;
+	} else {
+	    return start;
+	}
+    };
+}
+angular.filter("expandEdit", expand("Edit", "Done Editing"));
+angular.filter("expandShow", expand("Show", "Hide"));
--- app/assets/javascripts/widgets.js
+++ app/assets/javascripts/widgets.js
@@ -1,1 +1,8 @@
 /* http://docs.angularjs.org/#!angular.widget */
+angular.widget('kdp:category', function(compileElement) {
+    var compiler = this;
+    var category = compileElement.attr('category');
+    return function(linkElement) {
+	var currentScope = this;
+    }
+});
